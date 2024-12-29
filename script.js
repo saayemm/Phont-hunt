9d0debd47b735961f49f49c29d27da25b328a0b0
Saayemm <sayemdcc@gmail.com>
2024-12-29 21:23:39 +0600

    search btn

--- script.js
+++ script.js
@@ -7,11 +7,33 @@ const loadPhone = async() => {
     
 }
 const displayPhones = phones => {
+
+    const phoneCOntainer = document.getElementById("phone-container")
+
     phones.forEach(phone=>{
-        console.log(phone);
-        
+    console.log(phone);
+
+    const PhoneDiv = document.createElement("div")
+    PhoneDiv.classList = `card card-compact bg-base-100 w-96 shadow-xl bg-white text-black`
+    PhoneDiv.innerHTML = `
+            <figure>
+                <img src="${phone.image}"
+                    alt="Shoes" class="mt-4" />
+                    </figure>
+                    <div class="card-body flex flex-col justify-center items-center gap-2 p-4">
+                         <h2 class="card-title">${phone.phone_name}</h2>
+                         <p>${phone.slug}</p>
+                         <div class="card-actions flex w-full justify-center">
+                            <button class="btn btn-primary">Buy Now</button>
+                        </div>
+                    </div>
+    ` 
+    phoneCOntainer.appendChild(PhoneDiv)
     })
 }
-
-
 loadPhone()
+
+const hundleSearch = () => {
+    console.log("see mobile");
+    
+}
