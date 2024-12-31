1a10f4abcc64c8e9868e0784c275dd7143fc6439
Saayemm <sayemdcc@gmail.com>
2024-12-31 12:20:18 +0600

    phone show

--- script.js
+++ script.js
@@ -12,16 +12,16 @@ const displayPhones = phones => {
     phones.forEach(phone => {
         console.log(phone);
         const phoneCard = document.createElement("div")
-        phoneCard.classList = `card bg-base-100 p-4 shadow-xl`
+        phoneCard.classList = `card bg-white text-black m-2 p-4 shadow-xl`
         phoneCard.innerHTML = `
              <figure class="px-10 pt-10">
                 <img
-                    src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                    src="${phone.image}"
                     alt="Shoes"
                     class="rounded-xl" />
             </figure>
                 <div class="card-body items-center text-center">
-                     <h2 class="card-title">Shoes!</h2>
+                     <h2 class="card-title">${phone.phone_name}</h2>
                     <p>If a dog chews shoes whose shoes does he choose?</p>
                     <div class="card-actions">
                         <button class="btn btn-primary">Buy Now</button>
